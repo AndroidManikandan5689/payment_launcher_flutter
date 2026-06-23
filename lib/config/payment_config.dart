bc3486b08d6251c2767d273bad39818f1cd72f51
Manikandan Kasinathan <16814209+AndroidManikandan5689@users.noreply.github.com>
2026-06-23 16:08:39 +0530

    Update payment_config.dart

--- lib/config/payment_config.dart
+++ lib/config/payment_config.dart
@@ -3,6 +3,6 @@ class PaymentConfig {
   static const String merchantName = 'Demo Merchant';
   static const String amount = '1.00';
 
-  static const String aaniPayDeepLink = 'aani://pay';
+  static const String aaniPayDeepLink = 'aani://request_to_pay_payment?idPayment=125050';
   static const String aaniPayStoreUrl = 'https://play.google.com/store/apps/details?id=ae.aletihadpayments.aani&hl=en_IN';
 }
